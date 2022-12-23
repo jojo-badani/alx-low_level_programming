5913b77dbcb74a322189d026f98b13ee0816333d
jojo-badani <joshuaoluwatoyin068@gmail.com>
2022-12-22 17:52:57 -0800

    update 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @src: copy from
  * Return:void
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -23,6 +23,7 @@ char *_strcat(char *dest, char *src);
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
