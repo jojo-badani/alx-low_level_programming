a19774911dc9cd5d0bcb1c27d5cfc5d88147d739
jojo-badani <joshuaoluwatoyin068@gmail.com>
2022-12-23 16:50:53 -0800

    update on task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,10 +13,10 @@ void print_number(int n)
 	n1 = n;
 	if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 		n1 = -n;
 	}
-	if (n1 / 10 != 0)
+	if ((n1 / 10) != 0)
 	{
 		print_number(n1 / 10);
 	}
