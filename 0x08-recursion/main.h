68a10c4a19901325988448921516a3fc7416fbd8
jojo-badani <joshuaoluwatoyin068@gmail.com>
2023-01-04 10:50:42 -0800

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,5 +17,10 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 #endif
