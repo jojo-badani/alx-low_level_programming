47843f6aa1185c73a93f1c6a2d73e986ce5a9fdc
jojo-badani <joshuaoluwatoyin068@gmail.com>
2023-01-04 09:44:07 -0800

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,16 +6,14 @@
  *
  * Return: The length of the string.
  */
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s);
 {
 	int len = 0;
 
 	if (*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (len);
 }
